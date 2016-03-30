3567516db36e591e2b1aa627a53da2d36ec79020
Matt <mwall@csumb.edu>
2016-03-29 21:04:09 -0700

    swapped out old files for new ones

--- src/Main.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-import MainScreen
-
-root = Tk()
-ms = MainScreen.Dialog(root)
-
-
--- src/MainScreen.py
+++ src/MainScreen.py
@@ -1,37 +1,37 @@
 from tkinter import *
 from tkinter import filedialog
+from YouTubeAPI import *
 
 
-class Dialog:
+class MainScreen:
+
     def __init__(self, master):
-        directory = ""
+        self.directory = ""
 
         master.minsize(width=600, height=400)
         #master.configure(background="firebrick3")
         frame = Frame(master)
 
-        #create labels
         urlLabel = Label(frame, text="URL")
         artistLabel = Label(frame, text="Artist")
         albumLabel = Label(frame, text="Album")
         titleLabel = Label(frame, text="Title")
         directoryLabel = Label(frame, text="Directory")
 
-        #create entry boxes
+
         urlEntry = Entry(frame, width=50)
         artistEntry = Entry(frame, width=50)
         albumEntry = Entry(frame, width=50)
+        albumEntry.insert(0, "YouTube")
         titleEntry = Entry(frame, width=50)
         directoryEntry = Entry(frame, width=50)
 
-        #place labels on grid
         urlLabel.grid(row=0, sticky=E)
         artistLabel.grid(row=1, sticky=E)
         albumLabel.grid(row=2, sticky=E)
         titleLabel.grid(row=3, sticky=E)
         directoryLabel.grid(row=4, sticky=E)
 
-        #place entry boxes on grid
         urlEntry.grid(row=0, column=1)
         artistEntry.grid(row=1, column=1)
         albumEntry.grid(row=2, column=1)
@@ -47,30 +47,35 @@ class Dialog:
         quitButton = Button(frame, text="Quit", command=frame.quit)
         quitButton.grid(row=5, columnspan=4)
 
+
         frame.pack()
+
         frame.mainloop()
 
     def askDirectory(self):
-        self.directory = filedialog.askdirectory()
+        directory = filedialog.askdirectory()
         self.directoryEntry.insert(0, self.directory)
 
     def submit(self):
-        options =   {'url': self.urlEntry.get(),
+        url = self.urlEntry.get()
+        #testYDL = Ydl(url, setOptions())
+        #downloadVideo(testYDL)
+        userInfo = {'url': self.urlEntry.get(),
                     'artist': self.artistEntry.get(),
-                    'album' : self.albumEntry.get(),
-                    'title' : self.titleEntry.get(),
-                    'directory': self.directyEntry.get()
+                    'album': self.albumEntry.get(),
+                    'title': self.titleEntry.get(),
+                    'directory': self.directoryEntry.get()
                     }
-        options.cleanData(self)
 
-    def cleanData(options):
-        for entry in options:
-            if entry == None:
-                entry = ""
+        YouTubeAPI(userInfo)
 
 
+#root = Tk()
 
+#app = MainScreen(root)
 
+# keeps the window on the screen
+#root.mainloop()
 
 
 
